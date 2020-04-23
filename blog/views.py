d3bef3a1e46defba6b6fefe5ff575b6a1dc20e16
Joselino Santos <joselinosantosti@gmail.com>
2020-04-22 23:47:12 -0300

    Template alterado para exibir os posts do banco de dados

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # As views
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+	return render(request, 'blog/post_list.html', {'posts': posts})
 	
