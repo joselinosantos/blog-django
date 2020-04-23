5c6a023f53f65db3332758bb705d0c793aa4a013
Joselino Santos <joselinosantosti@gmail.com>
2020-04-23 11:54:15 -0300

    Alterações nos links e template detalhes

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list')
+	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detalhes, name='post_detalhes')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
-# As views
+# Função para as views
 def post_list(request):
 	posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
 	return render(request, 'blog/post_list.html', {'posts': posts})
-	
+	#Post.objects.get(pk=pk)
+
+def post_detalhes(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detalhes.html', {'post': post})
