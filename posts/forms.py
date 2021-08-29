4a2adc96d0dd979124f53c094582d56d0b23caba
Joselino Santos <joselinosantosti@gmail.com>
2021-08-29 10:23:20 -0300

    New field categorias

--- posts/forms.py
+++ posts/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ('titulo','texto','imagem')
+		fields = ('titulo', 'categoria', 'texto','imagem')
--- posts/models.py
+++ posts/models.py
@@ -3,10 +3,18 @@ from django.conf import settings
 from django.utils import timezone
 
 class Post(models.Model):
+	CATEGORIAS = (
+		('programacao', 'Python'),
+		('raspberry', 'Raspberry Pi'),
+		('sql', 'SQL'),
+		('web', 'Web'),
+		('datascience', 'DataScience')
+	)
 	titulo = models.CharField(max_length=200)
 	autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	imagem = models.CharField(max_length=20)
 	texto = models.TextField()
+	categoria = models.CharField(max_length=11, choices=CATEGORIAS)
 	data_criacao = models.DateTimeField(default=timezone.now)
 	data_publicacao = models.DateTimeField(blank=True, null=True)
 
